372a18a47e33bf71d222c31ba9164ca457246e7e
Tomás Neves <2201747@my.ipleiria.pt>
2023-05-05 00:46:06 +0100

    Create code initialization

--- happyguest-web/src/components/CardBoxsCustom/CardBoxCode.vue
+++ happyguest-web/src/components/CardBoxsCustom/CardBoxCode.vue
@@ -1,15 +1,19 @@
 <script setup>
-import {  watch,ref, watchEffect} from "vue";
+import { watch, ref, watchEffect } from "vue";
 import {
     mdiContentSaveCheck,
     mdiRename,
     mdiBarcode,
     mdiBed,
-    mdiCalendarRange
+    mdiCalendarRange,
+    mdiPencil,
+    mdiRefresh,
 } from "@mdi/js";
 import CardBoxModal from "@/components/CardBoxs/CardBoxModal.vue";
 import FormControl from "@/components/Forms/FormControl.vue";
 import FormField from "@/components/Forms/FormField.vue";
+import BaseButtons from "@/components/Bases/BaseButtons.vue";
+import BaseButton from "@/components/Bases/BaseButton.vue";
 import { useCodeStore } from "@/stores/code";
 
 const codeStore = useCodeStore();
@@ -31,79 +35,136 @@ watchEffect(() => {
     isModalActive.value = props.active;
 });
 
-
 watch(
     () => props.selected,
     async (value) => {
         code.value = await codeStore.getCode(value);
-        editForm.value.code = code.value?.code;
-        editForm.value.rooms = code.value?.rooms;
-        editForm.value.entry_date = code.value?.entry_date;
-        editForm.value.exit_date = code.value?.exit_date;
-
+        Form.value.code = code.value?.code;
+        Form.value.rooms = code.value?.rooms;
+        Form.value.entry_date = code.value?.entry_date;
+        Form.value.exit_date = code.value?.exit_date;
     }
 );
 
-const editForm = ref({
-    code: "",
+const randomString = () => {
+    const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZ";
+    const string_length = 4;
+    let randomstring = "";
+    for (let i = 0; i < string_length; i++) {
+        let rnum = Math.floor(Math.random() * chars.length);
+        randomstring += chars.substring(rnum, rnum + 1);
+    }
+    return randomstring;
+};
+
+const Form = ref({
+    code: props.selected ? "" : randomString(),
     rooms: "",
     entry_date: "",
     exit_date: "",
 });
 
 const emit = defineEmits(["update:active"]);
+
+const getDateString = (date) => {
+    var dateString = new Date(date);
+    return (
+        dateString.getFullYear() +
+        '-' +
+        (dateString.getMonth() > 8
+            ? dateString.getMonth() + 1
+            : "0" + (dateString.getMonth() + 1)) +
+            '-' +
+        (dateString.getDate() > 9
+            ? dateString.getDate()
+            : "0" + dateString.getDate())
+    );
+};
+
+const createCode = async () => {
+    /*var response = await codeStore.create(Form.value);
+    if (response) {
+        //isSuccessNotifActive.value = true;
+        //clearFields();
+        //ocurrenceStore.clearStore();
+        emit('update:active', true)
+        setTimeout(function () {
+            //isSuccessNotifActive.value = false;
+            router.go(-1);
+        }, 3000);
+    }*/
+};
+const editCode = async () => {
+    //
+};
+
+
 </script>
 
 <template>
     <CardBoxModal
         v-model="isModalActive"
-        :title="'Editar Código ➯ ' + code?.code"
-        button-label="Atualizar"
+        :title="selected ? 'Editar Código ➯ ' + code?.code : 'Criar Código'"
+        :button-label="selected ? 'Atualizar' : 'Criar'"
         :icon="mdiContentSaveCheck"
-        :icon-title="mdiRename"
+        :icon-title="selected ? mdiRename : mdiPencil"
         button="success"
         @cancel="emit('update:active', false)"
+        @confirm=" selected ? editCode : createCode"
         has-errors
         has-cancel
         has-close
     >
-        <FormField
-            label="Código"
-            class="mt-6"
-        >
+        <FormField label="Código" class="mt-6">
             <FormControl
-                v-model="editForm.code"
+                v-model="Form.code"
                 :icon="mdiBarcode"
                 name="code"
                 required
             />
+            <BaseButtons v-if="!selected">
+                <BaseButton
+                    color="info"
+                    :icon="mdiRefresh"
+                    small
+                    @click="Form.code = randomString()"
+                />
+            </BaseButtons>
         </FormField>
         <FormField label="Quarto(s)" help="Quarto. Obrigatório.">
             <FormControl
-                v-model="editForm.rooms"
+                v-model="Form.rooms"
                 :icon="mdiBed"
                 name="rooms"
                 required
             />
         </FormField>
         <FormField>
-            <FormField label="Data de entrada" help="A data de entrada. Obrigatório.">
-            <FormControl
-                v-model="editForm.entry_date"
-                :icon="mdiCalendarRange"
-                name="entry_date"
-                required
-            />
+            <FormField
+                label="Data de entrada"
+                help="A data de entrada. Obrigatório."
+            >
+                <FormControl
+                    :modelValue="getDateString(Form.entry_date)"
+                    :icon="mdiCalendarRange"
+                    name="entry_date"
+                    type="date"
+                    required
+                />
             </FormField>
 
-            <FormField label="Data de saida" help="A data de saida. Obrigatório.">
-            <FormControl
-                v-model="editForm.exit_date"
-                :icon="mdiCalendarRange"
-                name="exit_date"
-                required
-            />
-        </FormField>
-        </FormField>
+            <FormField
+                label="Data de saida"
+                help="A data de saida. Obrigatório."
+            >
+                <FormControl
+                    :modelValue="getDateString(Form.exit_date)"
+                    :icon="mdiCalendarRange"
+                    name="exit_date"
+                    type="date"
+                    required
+                />
+            </FormField>
+        </FormField> 
     </CardBoxModal>
 </template>
--- happyguest-web/src/views/Dashboard/CodesView.vue
+++ happyguest-web/src/views/Dashboard/CodesView.vue
@@ -8,12 +8,15 @@ import BaseButton from "@/components/Bases/BaseButton.vue";
 import CardBoxComponentEmpty from "@/components/CardBoxs/CardBoxComponentEmpty.vue";
 import TableCodes from "@/components/Tables/TableCodes.vue";
 import CardBox from "@/components/CardBoxs/CardBox.vue";
+import CardBoxCode from "@/components/CardBoxsCustom/CardBoxCode.vue";
 import { onMounted, ref } from "vue";
 import { useCodeStore } from "@/stores/code";
 
 const codeStore = useCodeStore();
 
 const hasCodes = ref(false);
+
+const isModalActiveCreate = ref(false);
 
 onMounted(() => {
     hasCodes.value = codeStore.getCodes(0);
@@ -23,17 +26,22 @@ onMounted(() => {
 </script>
 
 <template>
+     <CardBoxCode
+        :active="isModalActiveCreate"
+        @update:active="isModalActiveCreate = $event"
+    />
     <LayoutAuthenticated>
         <SectionMain>
             <SectionTitleLineWithButton
                 :icon="mdiBarcode"
                 :title="'Códigos'"
+                @click="isModalActiveCreate = true;"
                 main
             >
                 <BaseButtons>
                     <BaseButton
                         :icon="mdiPlus"
-                        label="Associar Código"
+                        label="Criar Código"
                         color="success"
                         rounded-full
                         small
